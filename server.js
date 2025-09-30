026e3e752e5de563100d9ef33685f9b8c4cebec9
Dylan Tai <dylztai@gmail.com>
2025-09-30 14:41:55 -0400

    organized mongoose in server.js

--- server.js
+++ server.js
@@ -13,13 +13,6 @@ import authRouter from "./controllers/auth.js";
 import usersRouter from "./controllers/users.js";
 import moodRouter from "./routes/mood.js";
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI);
-
-mongoose.connection.on("connected", () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
-
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -30,7 +23,13 @@ app.use("/auth", authRouter);
 app.use("/users", usersRouter);
 app.use("/moods", moodRouter);
 
-// Start the server and listen on port 3000
-app.listen(3000, () => {
-  console.log("The express app is ready!");
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  // Start the server and listen on port 3000
+  app.listen(3000, () => {
+    console.log("The express app is ready!");
+  });
 });
